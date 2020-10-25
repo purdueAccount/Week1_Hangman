8cf0a776745841150019fcb0c4501cc0b5d67180
purdueAccount <69224463+purdueAccount@users.noreply.github.com>
2020-10-25 12:10:41 -0400

    create Index landing page
    create Index landing page

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Hangman</title>
+    <meta charset="utf-8">
+	
+    <!-- Always force latest IE rendering engine or request Chrome Frame -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+    <!-- description and keywords -->
+    <meta name="description" content="Hangman, the simple guessing game enjoyed by children of all ages." />
+    <meta name="keywords" content="hangman, game, search, word, english, language, fun" />
+	
+    <!-- external document - links and scripts -->
+    <link href="/css/adjust.css" rel="stylesheet" type="text/css" />
+    <link href="/css/style.css" rel="stylesheet" type="text/css" />
+    <script src="/js/script.js" type="text/javascript"></script>
+</head>
+
+<body>
+
+<header>
+	<nav></nav>
+</header>
+
+<main>
+	<h1>Hangman</h1>
+</main>
+
+<footer>
+	<!-- JS Libraries -->
+	<script src="/js/libs/lib.js"></script>
+</footer>
+
+</body>
+
+</html>
